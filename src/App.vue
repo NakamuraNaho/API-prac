56057cec1135d7a2663a316512fcee0413195901
Nakamura Naho <nahoichi323@gmail.com>
2021-04-17 20:42:11 +0900

    3commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <input type="text" v-model="coad" value="zipcoadを取得">
     <button @click="zipcoad(coad)">住所自動入力</button>
-    <p>Adress:{{ postcode }}</p>
+    <p>Adress:{{ this.address }}</p>
 
   </div>
   
@@ -14,27 +14,32 @@ export default{
   data(){
     return{
       coad:"",
-      postcode:""
-      
+      address:"",
     }
 
   },
 
   methods:{
-    zipcoad(){
-      const url = "https://apis.postcode-jp.com/api/v4/" + this.coad + '?apiKey=' + "ViYTWVzJn5CupAeLkQRxCuxs4TtSZPQLZaMBaBw";
+    
+    async zipcoad(){
+      const url = `https://apis.postcode-jp.com/api/v4/postcodes/${this.coad}?apiKey=ViYTWVzJn5CupAeLkQRxCuxs4TtSZPQLZaMBaBw`;
 
-      axios.get(url).then(response => {this.result = response.postcode})
-       
+      axios.get(url).then((response) => { 
+      
+       this.address = response.data[0].allAddress
+       console.log(response)
+       response.data[0].allAddress});
 
       
 
+
+      
+      
+      
     }
 
     }
 
   }
-
-
 
 </script>
